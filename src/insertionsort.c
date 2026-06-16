c55312e2efefdda4a4c64291856c9850d119542c
pierool2000 <pierrik.clary@epitech.eu>
2026-06-16 11:53:51 +0200

    insersort no test

--- src/insertionsort.c
+++ src/insertionsort.c
@@ -1,31 +1,92 @@
 #include "../include/include.h"
 
+
+// static int sort(int (*cmp)(file_stock_t*, file_stock_t*)) {
+//     file_stock_t *t_cur;
+
+//     t_cur = current->prev;
+//     while (t_cur->prev != NULL) {
+//         printf("\n___\n%s >< %s\n___\n", temp->word, t_cur->word);
+//         if (cmp(temp, t_cur) > 0) {
+//             next = t_cur->next;
+//             temp->next = next;
+//             if (next != NULL)
+//                 next->prev = temp;
+//             t_cur->next = temp;
+//             temp->prev = t_cur;
+//             return 0;
+//         }
+//         t_cur = t_cur->prev;
+//     }
+//     if (t_cur->prev == NULL) {
+//         printf("\n\ntcur etait NULL\n\n");
+//         t_cur->prev = temp;
+//         temp->next = t_cur;
+//         temp->prev = NULL;
+//     }
+//     return 1;
+// }
+
+// static int sort(int (*cmp)(file_stock_t*, file_stock_t*)) {
+//     tcur = current->prev;
+//     printf("\nnumber 1\n%s\n\n", tcur->word);
+//     while (tcur->prev != NULL && cmp(tcur, temp) > 0)
+//         tcur = tcur->prev;
+//     printf("\nnumber 2\n%s\n\n", tcur->word);
+//     if (tcur->prev == NULL) {
+//         temp->next = tcur;
+//         tcur->prev = temp;
+//         temp->prev = NULL;
+//         return 1;
+//     } else if (cmp(tcur, temp) > 0) {
+//         prev = tcur->prev;
+//         prev->next = temp;
+//         temp->prev = prev;
+//         tcur->prev = temp;
+//         temp->next = tcur;
+//         return 0;
+//     } else if (cmp(temp, tcur) > 0) {
+//         next = tcur->next;
+//         next->prev = temp;
+//         temp->next = next;
+//         tcur->next = temp;
+//         temp->prev = tcur;
+//         return 0;
+//     }
+//     return 0;
+// }
+
 static file_stock_t *current;
 static file_stock_t *temp;
 static file_stock_t *next;
+static file_stock_t *tcur;
+static file_stock_t *prev;
 
-static int sort(int (*cmp)(file_stock_t*, file_stock_t*)) {
-    file_stock_t *t_cur;
 
-    t_cur = current;
-    do {
-        if (cmp(temp, t_cur) > 0) {
-            next = t_cur->next;
-            temp->next = next;
-            if (next != NULL)
-                next->prev = temp;
-            t_cur->next = temp;
-            temp->prev = t_cur;
-            return 0;
-        }
-        t_cur = t_cur->prev;
-    } while (t_cur->prev != NULL);
-    if (t_cur->prev == NULL) {
-        t_cur->prev = temp;
-        temp->next = t_cur;
+static int sort(int (*cmp)(file_stock_t*, file_stock_t*))
+{
+    tcur = current->prev;
+
+    // On recule tant que tcur doit venir APRÈS temp
+    while (tcur->prev != NULL && cmp(tcur, temp) > 0)
+        tcur = tcur->prev;
+
+    if (cmp(tcur, temp) > 0) {
+        // temp se place AVANT tcur (tcur devient la nouvelle tête)
+        temp->next = tcur;
         temp->prev = NULL;
+        tcur->prev = temp;
+        return 1; // nouvelle tête
+    } else {
+        // temp se place APRÈS tcur
+        next = tcur->next;
+        tcur->next = temp;
+        temp->prev = tcur;
+        temp->next = next;
+        if (next != NULL)
+            next->prev = temp;
+        return 0;
     }
-    return 1;
 }
 
 int insertionsort(file_stock_t **head, char *mode) {
@@ -48,9 +109,9 @@ int insertionsort(file_stock_t **head, char *mode) {
                 next->prev = current;
             if (sort(cmp) == 1)
                 *head = temp;
-            current = current->next;
         } else
             current = current->next;
+        printf("\t%s\n", current->word);
         printlist(*head);
         printf("---\n");
     }
